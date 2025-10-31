d2c248017546c40fc9220b4faefeb08494cc3fbc
greemaltend4 <dengestabiy@hotmail.com>
2025-10-31 09:09:10 +0000

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue dans Intégration Cloud Salons !');
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution à http://localhost:${port}`);
+});
