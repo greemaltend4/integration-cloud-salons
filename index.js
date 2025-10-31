23a50914524545d3f5480bc1a1fa046b90a890f8
greemaltend4 <dengestabiy@hotmail.com>
2025-10-31 09:11:54 +0000

    Fix indentation and improve French message consistency.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Bienvenue dans l\'Intégration Cloud Salons !');
+    res.send('Bienvenue dans l\'intégration Cloud Salons !');
 });
 
 app.listen(port, () => {
